16a54a3f1ea65e0e5314f979243d772b2c8c37e1
Svetlana <medjog@gmail.com>
2023-04-14 17:56:00 +0400

    Решение задачи

--- Num01/Program.cs
+++ Num01/Program.cs
@@ -3,3 +3,45 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк массива: ");
+int m = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int n = int.Parse(ReadLine());
+int[,] array = FillArray(m,n);
+PrintArray(array);
+
+
+int[,] FillArray(int m, int n)
+{
+    int[,] Array = new int[m, n];
+    for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+        {
+             Array[i, j] = new Random().Next(-10,10);
+        }
+        }
+        return Array;
+}
+void PrintArray(int[,] Arr)
+{
+    for (int i = 0; i < Arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < Arr.GetLength(1); j++)
+                {
+                    Write($"{Arr[i,j]} ");
+                }
+            WriteLine();
+        }
+}
+
+
+
+
+
+
