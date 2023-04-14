35bec1708e37fc65b12f7029241be1c20e1198b7
Svetlana <medjog@gmail.com>
2023-04-14 20:32:22 +0400

    Решение задачи 2

--- Num02/Program.cs
+++ Num02/Program.cs
@@ -1,11 +1,52 @@
-﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
 Например, задан массив:
-
 1 4 7 2
-
 5 9 2 3
-
 8 4 2 4
-
 17 -> такого числа в массиве нет */
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+             matr[i,j] = new Random().Next(1,10);
+        }
+    }
+} 
+
+void PrintArray(int[,] matr) 
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+} 
+
+void Elements(int[,] matr, int i, int j)
+{
+    int row = matr.GetLength(0);
+    int column = matr.GetLength(1);
+    if (i < row && j < column)
+    {
+        Console.WriteLine($"Элемент на позиции [{i},{j}] --> {matr[i,j]}");
+    }
+
+    else
+    {
+        Console.WriteLine("Такого элемента в массиве нет");
+    }
+}
+
+int[,] Array = new int[5,5];
+FillArray(Array);
+PrintArray(Array);
+Elements(Array, 3, 4);
+
+ 
